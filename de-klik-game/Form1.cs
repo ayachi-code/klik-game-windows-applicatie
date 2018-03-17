1550ff2ddcb81f797b573dc5e36b1f1600bee4c6
bilal <bilal.ayachi@gmail.com>
2018-03-17 19:07:26 +0100

    String variable is gelijk aan de text waarde van normaal

--- de-klik-game/Form1.cs
+++ de-klik-game/Form1.cs
@@ -12,6 +12,7 @@ namespace de_klik_game
 {
     public partial class Form1 : Form
     {
+      
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +20,8 @@ namespace de_klik_game
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            String normaal_klik = normaal.Text;
+           
         }
     }
 }
