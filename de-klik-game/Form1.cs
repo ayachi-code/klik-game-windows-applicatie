e024c818ee3122deb17e136abb5df2c239ff3629
bilal <bilal.ayachi@gmail.com>
2018-03-17 19:04:16 +0100

    calback functie gemaakt voor klik knop

--- de-klik-game/Form1.cs
+++ de-klik-game/Form1.cs
@@ -16,5 +16,10 @@ namespace de_klik_game
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
